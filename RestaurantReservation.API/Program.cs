cdc748cd1528de25d680b258d7430a4e6387e8ba
Nelson Ortega <nelson.ortega@foothillsolutions.com>
2024-03-14 15:51:20 -0500

    feat(db): implemented db connection settings

--- RestaurantReservation.API/Program.cs
+++ RestaurantReservation.API/Program.cs
@@ -1,4 +1,9 @@
+using RestaurantReservation.API.Settings;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.Configure<MongoDbSettings>(builder.Configuration.GetSection("RestaurantReservationDb"));
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
